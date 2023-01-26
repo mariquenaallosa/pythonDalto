78b3d66c014cbe72969685b089f0f1ae7fbd9614
Mariquena <allosamariquena@gmail.com>
2023-01-26 15:23:21 -0300

    Initial commit

---
+++ 1-datos-simples.py
@@ -0,0 +1,28 @@
+# Tipos de datos
+
+# -> Texto
+# en una línea
+texto = "String"
+texto2 = 'String'
+
+# varias líneas -> similar a ` ` en JS
+texto3 = """Sus datos son: 
+            nombre : Lucas
+            apellido: Dalto"""
+
+texto4 = '''Sus datos son: 
+            nombre : Lucas
+            apellido: Dalto'''
+
+
+# Numeros
+#  -> INT
+numero = 4
+
+# -> float
+numero2 = 1.2
+
+#  -> Boolean 
+
+boolean = False 
+boolean2 = True 
---
+++ 2-variables.py
@@ -0,0 +1,19 @@
+a = 2 
+b = 3
+
+c = a + b
+
+print(c)
+
+
+nombre = "Lucas"
+
+print(nombre)
+
+
+
+# fStrings -> para concatenar sin problemas de tipos de datos
+
+bienvenida = f"Hola  {nombre} ¿Cómo estás?"
+
+print(bienvenida)

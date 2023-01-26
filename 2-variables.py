44656a06fb98ef0e6a77ecba52a5c57ef90db8c2
Mariquena <allosamariquena@gmail.com>
2023-01-26 16:15:11 -0300

    Update 2-variables.py

--- 2-variables.py
+++ 2-variables.py
@@ -5,15 +5,31 @@ c = a + b
 
 print(c)
 
+# Definiendoo una variable con camelCase
+nombreCompleto = "Lucas Dalto"
+print(nombreCompleto)
 
-nombre = "Lucas"
+# Definiendoo una variable con sanke_case (es el recomendable)
+nombre_completo = "Lucas"
+print(nombre_completo)
 
-print(nombre)
 
 
+# concatenar con fStrings -> para concatenar sin problemas de tipos de datos
 
-# fStrings -> para concatenar sin problemas de tipos de datos
-
-bienvenida = f"Hola  {nombre} ¿Cómo estás?"
+bienvenida = f"Hola {nombreCompleto} ¿Cómo estás?"
 
 print(bienvenida)
+
+# concatenar con  + 
+bienvenida = "Hola " + "¿Cómo estás?"
+
+print(bienvenida)
+
+# operadores de pertenecia (in - not in)
+print("ola" in bienvenida) #true
+print("pedro" in bienvenida) #false
+print("pedro" not in bienvenida) #true
+
+# Borrar datos -> Hacer que una variable no esté más declarada 
+del bienvenida

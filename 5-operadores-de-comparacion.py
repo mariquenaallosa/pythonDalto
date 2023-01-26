46fc8e028f941ad6f0781142f0179d61173bedf3
Mariquena <allosamariquena@gmail.com>
2023-01-26 18:36:17 -0300

    Create 5-operadores-de-comparacion.py

---
+++ 5-operadores-de-comparacion.py
@@ -0,0 +1,36 @@
+#   Operadoradores de comparación
+
+# igual ==
+igual_que = 5 == 6  # false
+
+# distinto !=
+distinto_de = 5 != 6  # true
+
+# menor que <
+menor = 5 < 6  # true
+
+# menor o igual <=
+menor_igual = 5 <= 6  # false
+
+# mayor que >
+mayor = 5 > 6  # false
+
+# mayor o igual >=
+mayor_igual = 5 >= 6  # false
+
+# caluclos combinados
+a = 5
+b = 10
+c = 20
+
+comparacion = a + b == c
+print(comparacion)
+
+#comparar usuarios 
+
+usuario_bd = "Lucas Dalto"
+usuario_ingresado = "Lucas"
+
+verificar = usuario_bd == usuario_ingresado
+
+print(verificar)

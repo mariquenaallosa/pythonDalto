3181e7d38bac31329bbf88b43b7001e317134fdd
Mariquena <allosamariquena@gmail.com>
2023-01-26 18:43:08 -0300

    if-else

---
+++ 6-condicionales-id-else.py
@@ -0,0 +1,15 @@
+edad = 17 
+
+if edad >= 18:
+    print("Es mayor de edad, puede pasar")
+else:
+    print(f"Tiene {edad} años, es menor de edad")
+
+
+contrasena_almacenada = "Admin6696"
+contrasena_ingresada = "Admin"
+
+if contrasena_almacenada == contrasena_ingresada:
+    print("Iniciando sesion")
+else:
+    print("Contraseña incorrecta, intente nuevamente")

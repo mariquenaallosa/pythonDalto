42de2df5ff5b0074b756fcdc19eab68db71516cc
Mariquena <allosamariquena@gmail.com>
2023-01-26 18:27:23 -0300

    Create 4-operadores-aritmetics.py

---
+++ 4-operadores-aritmetics.py
@@ -0,0 +1,30 @@
+# Suma + y Resta - 
+suma = 12+5
+resta= 12-5
+
+# Multi * y Divi /
+
+multi = 4*2
+division = 4/2 # Nos devuelve un float 
+
+#Potencia **
+potencia = 2**4
+
+# Division baja //
+# -> devuelve entero redondeado hacia abajo
+division_baja = 12//5 
+
+# Resto % 
+resto = 5%2
+
+# type() -> devuelve el tipo de dato 
+tipo_de_datos = type(5)
+print(tipo_de_datos)
+
+
+print(suma)
+print(resta)
+print(multi)
+print(division)
+print(division_baja)
+print(resto)

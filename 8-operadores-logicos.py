aae14b5b518d1b0a8fe4c037d2a05698311036c6
Mariquena <allosamariquena@gmail.com>
2023-01-26 19:21:39 -0300

    Create 8-operadores-logicos.py

---
+++ 8-operadores-logicos.py
@@ -0,0 +1,16 @@
+# OR 
+Resultado1 = True | False # True
+Resultado2 = True | True # True
+Resultado3 = False | True # True
+Resultado4 = False | False # False
+
+# AND
+Resultado5 = True & False # False
+Resultado6 = True & True # True
+Resultado7 = False & True # False
+Resultado8 = False & False # False
+
+# NOT
+
+Resultado9 = not True  # False
+Resultado10 = not False # True

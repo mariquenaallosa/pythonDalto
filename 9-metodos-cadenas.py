c99aeccedfe3fa484d3aa341f04cb160d32dcb29
Mariquena <allosamariquena@gmail.com>
2023-01-27 16:11:45 -0300

    Update 9-metodos-cadenas.py

--- 9-metodos-cadenas.py
+++ 9-metodos-cadenas.py
@@ -24,14 +24,27 @@ numeric = cadena1.isnumeric()
 # espacio no es alfanumerico así que siempre que tenga espacios me tira False
 alphanumeric = cadena1.isalpha()
 # COUNT -> devuelve el número de ocurrencias de una subcadena dada.
-# nos dice cuantas veces encontró la coincidencia de
-count
+# nos dice cuantas veces encontró la coincidencia de una cadena dentro de otra cadena
+contar_coincidencia = cadena1.count("a")
 # LEN -> cuenta los caracteres de una cadena
+# FUNCION
+contar_len = len(cadena1)
 
-# ATARSWITH -> virifica si una cadena comienza con 
+# STARSWITH -> virifica si una cadena comienza con 
+empieza = cadena1.startswith("H")
 # ENDSWITH -> virifica si una cadena termina con 
+termina = cadena1.startswith("e")
+
 
 # REPLACE -> reemplaza un valor por otro 
-# SPLIT -> separa por un parametro dado
+# cadena_nueva = cadena_vieja.replace(viejo, nuevo)
+cadena_nueva = cadena1.replace("la", "lu")
+cadena_nueva = cadena1.replace(",", " ")
+cadena_nueva_2= cadena_nueva.capitalize()
 
-print(alphanumeric)
+# SPLIT -> separa por un parametro dado
+# nos devuelve una lista
+cadena_separada = cadena1.split(",")
+
+
+print(cadena_separada)

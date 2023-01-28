5016b5082da1df120836008754e0aba6bc01aa98
Mariquena <allosamariquena@gmail.com>
2023-01-28 01:40:06 -0300

    Create 16-tuplas.py

---
+++ variables 2.0/16-tuplas.py
@@ -0,0 +1,17 @@
+# Crear tuplas con tuple()
+tupla = tuple(["dato1","dato2"])
+print(tupla)
+
+# Crear tupla sin () -> equivalente a crear tupla con ()
+tupla2 = "dato1", "dato2"
+print(tupla2)
+
+
+
+# Crear tupla de un sólo dato 
+tupla3 = "dato1",
+print(tupla3)
+
+
+# ¿Cúndo utilizar TUPLAS ?
+# -> cuando son datos de sólo lectura

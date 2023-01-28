82655bee15fa13c3282f3e9bd000e59fd1be9fe3
Mariquena <allosamariquena@gmail.com>
2023-01-28 00:24:06 -0300

    Create 12-data-inputs.py

---
+++ 12-data-inputs.py
@@ -0,0 +1,19 @@
+# La función INPUT -> nos devuelve simpre string
+# int() -> convierte en entero
+# float() -> convierte en flotante
+
+
+# pedirle un dato al usuario 
+nombre = input("Ingresa un Nombre:")
+
+# mostrando el dato ingresado 
+print(f"El nombre ingresado es {nombre}")
+
+
+# pedirle un número al usuario
+numero = input("Ingrese un número: ")
+
+#multiplicar numero 
+resultado = int(numero)*2
+
+print(resultado)

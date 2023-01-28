7f9b5b9b2258cb694ad1f8f6ee97039d348f6ad8
Mariquena <allosamariquena@gmail.com>
2023-01-28 01:40:03 -0300

    Update 15-desempaquetado.py

---
+++ variables 2.0/15-desempaquetado.py
@@ -0,0 +1,19 @@
+# funciona solo si la cantidad de variables es igual a la cantidad de datos
+# listas, tuplas y conjuntos(set)
+
+#crear datos
+datos_tupla = {"Lucas","Dalto",10000}
+datos_lista = ["Lucas","Dalto",10000]
+
+
+
+# desempaqutado
+nombre,apellido, subs = datos_tupla
+nombre2,apellido2, subs2 = datos_lista
+
+# mostrando datos 
+print(nombre,apellido,subs) # Lucas Dalto 10000
+print(nombre2,apellido2,subs2) # Lucas Dalto 10000
+
+
+

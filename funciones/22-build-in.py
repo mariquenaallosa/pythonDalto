1134b888efe59cbb9942ca26fb27a6217af96212
Mariquena <allosamariquena@gmail.com>
2023-01-29 17:40:37 -0300

    Create 22-build-in.py

---
+++ funciones/22-build-in.py
@@ -0,0 +1,20 @@
+numeros = [4,7,1,42,15]
+
+# Funciona con tuplas, listas, y sets 
+
+
+
+# encontrar el máximo valor de una lista
+numero_max= max(numeros)
+print(numero_max)
+
+# encontrar el minimo valor de una lista
+numero_min= min(numeros)
+print(numero_min)
+
+# redondear a x digitos 
+# round(numero,cantidad de decimales)
+# por defecto la casntidad de decimales es 0 
+
+numero = round(12.3456346,2)
+print(numero)

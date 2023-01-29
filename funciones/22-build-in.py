ed64f1ac653c7cdec4a878bd2f14818b881bec16
Mariquena <allosamariquena@gmail.com>
2023-01-29 18:24:18 -0300

    Update 22-build-in.py

--- funciones/22-build-in.py
+++ funciones/22-build-in.py
@@ -18,3 +18,27 @@ print(numero_min)
 
 numero = round(12.3456346,2)
 print(numero)
+
+
+# bool () 
+# retorna :
+#   False -> 0 , vacio , False , None
+#   True -> distinto de 0, True, cadena, datos no vacios
+
+resultado_bool = bool(0)
+print(resultado_bool)
+
+# all()
+# retorna :
+# True -> si todos los valores son verdaderos
+resultado_all = all([123,"true",[1,2,3]]) # verdadero
+print(resultado_all)
+resultado_all = all([0,"true",[1,2,3]]) # falso
+print(resultado_all)
+
+
+# sum()
+# -> suma todos los valores de un iterable
+
+resultado_sum = sum(numeros)
+print(resultado_sum)
